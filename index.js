cf206d34c4270307e9672d924c2b3f4c8650fc1d
mosraful21 <mosraful21@gmail.com>
2022-10-26 02:06:20 +0600

    course information data update

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ app.get('/courses', (req, res) => {
     res.send(categories)
 });
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const category_info = categories.filter( c=> c.id === id);
-    res.send(category_info);
-});
-
 app.get('/info/:id', (req, res) => {
     const id = req.params.id;
     const selectedInfo = courseInfo.find(info => info.id === id);
