5d3fe7ba4a79d2daa9713bd8148ac3f5d3be76a6
mosraful21 <mosraful21@gmail.com>
2022-10-25 17:17:58 +0600

    course-info data

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/course-category.json');
+const courseInfo = require('./data/course-info.json');
 
 app.get('/', (req, res) => {
     res.send("API Running");
 });
 
-app.get('/course-categories', (req, res) => {
+app.get('/courses', (req, res) => {
     res.send(categories)
-})
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_info = categories.filter( c=> c.id === id);
+    res.send(category_info);
+});
+
+app.get('/info/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedInfo = courseInfo.find(info => info.id === id);
+    res.send(selectedInfo);
+});
 
 app.listen(port, () => {
     console.log("server running on port", port);
-})
+});
